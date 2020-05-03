de70d6980f9f1b4154341a3187f8a82318b43219
JoseMiguel Marte <tumusicalatina@gmail.com>
2020-05-03 13:02:45 -0400

    finished the backend

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -26,9 +26,34 @@ router.route("/add").post((req, res, next) => {
     .then(() => res.json("todo added"))
     .catch((err) => res.status(400).json("Error: " + err));
 });
+
+// find a todo by id
+router.route("/:id").get((req, res, next) => {
+  Todos.findById(req.params.id)
+    .then((todo) => res.json(todo))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+// update a todo by id
+router.route("/update/:id").post((req, res, next) => {
+  Todos.findById(req.params.id)
+    .then((todo) => {
+      todo.username = req.body.username;
+      todo.action = req.body.action;
+      todo.datecompleted = Date.parse(req.body.date);
+      // console.log(todo.username);
+
+      todo
+        .save()
+        .then(() => res.json("Todo Updated!"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
 // remove/delete a todo
-router.route("/delete/:id").delete((req, res, next) => {
-  Todos.findByIdAndDelete("_id")
+router.route("/delete/:id").post((req, res, next) => {
+  Todos.findByIdAndDelete(req.params.id)
     .then(() => res.json("todo deleted!"))
     .catch((err) => res.status(400).json("Error: " + err));
 });
